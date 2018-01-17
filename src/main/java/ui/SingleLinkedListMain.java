d5bb3c070f002160cfc308eb7328408defaa7d10
Luke Buthman <lbuthman@gmail.com>
2018-01-16 18:42:34 -0600

    [refactor] Remove duplicate insert type

--- src/main/java/ui/SingleLinkedListMain.java
+++ src/main/java/ui/SingleLinkedListMain.java
@@ -31,18 +31,17 @@ public class SingleLinkedListMain {
         System.out.println("5. Insert at the end of the list");
         System.out.println("6. Insert after a specific node");
         System.out.println("7. Insert before a specific node");
-        System.out.println("8. Insert a specific position");
-        System.out.println("9. Delete the first node");
-        System.out.println("10. Delete the last node");
-        System.out.println("11. Delete a specific node");
-        System.out.println("12. Reverse the list");
-        System.out.println("13. Bubble sort by exchanging data");
-        System.out.println("14. Bubble sort by exchanging links");
-        System.out.println("15. Merge sort");
-        System.out.println("16. Insert Cycle");
-        System.out.println("17. Detect Cycle");
-        System.out.println("18. Remove Cycle");
-        System.out.println("19. Quit");
+        System.out.println("8. Delete the first node");
+        System.out.println("9. Delete the last node");
+        System.out.println("10. Delete a specific node");
+        System.out.println("11. Reverse the list");
+        System.out.println("12. Bubble sort by exchanging data");
+        System.out.println("13. Bubble sort by exchanging links");
+        System.out.println("14. Merge sort");
+        System.out.println("15. Insert Cycle");
+        System.out.println("16. Detect Cycle");
+        System.out.println("17. Remove Cycle");
+        System.out.println("18. Quit");
 
     }
 
@@ -100,8 +99,6 @@ public class SingleLinkedListMain {
             case 16:
                 break;
             case 17:
-                break;
-            case 18:
                 break;
         }
     }
